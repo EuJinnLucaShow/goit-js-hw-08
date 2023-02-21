7b74b65a662de11b69cda0c53fea17e36faf19c9
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-21 18:57:41 +0200

    Summary
    Додав бібліотеку SimpleLightbox як залежність проекту, використовуючи npm (посилання на CDN з моєї минулої роботи більше не потрібне).
    Використав свій JavaScript код з попередньої домашньої роботи, але виконав рефакторинг з урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryElements = document.querySelector('.gallery');
 
-console.log(galleryItems);
+// Формуємо розмітку галереї на основі масиву даних
+const createGalleryMarkup = galleryItems => {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+            </a>`;
+    })
+    .join('');
+};
+
+// Рендеримо розмітку галереї
+galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.5,
+    captionDelay: 250,
+    captionsData: 'alt',
+    fadeSpeed: 250,
+    rtl: true,
+});
+
